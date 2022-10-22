340445cc9230f774312b9a1a3d2900facd6d32a7
Алексей <lebedevalexeyo@gmail.com>
2022-10-22 19:28:25 +1100

    Добавьте файлы проекта.

---
+++ ArithProgression.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    internal class ArithProgression : ISeries
+    {
+        int startValue = 0;
+        int step = 1;
+        int currentValue;
+        public int GetNext()
+        {
+            currentValue += step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;
+            Console.WriteLine("Сброс. Стартовое значение: " + currentValue);
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+            Console.WriteLine("Стартовое значение: " + startValue);
+        }
+       public void setStep(int s)
+        {
+            step = s;
+            Console.WriteLine("Шаг: " + step );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression ap = new ArithProgression(); // Арифметическая прогрессия
+            ap.SetStart(12);
+            ap.setStep(10);
+            Console.WriteLine(ap.GetNext());//22
+            Console.WriteLine(ap.GetNext());//32
+            Console.WriteLine(ap.GetNext());//42
+            Console.WriteLine(ap.GetNext());//52
+            ap.Reset();
+            Console.WriteLine(ap.GetNext());//22
+
+            GeomProgression gp = new GeomProgression(); //Геометрическая прогрессия
+            gp.SetStart(1);
+            gp.setStep(2);
+            Console.WriteLine(gp.GetNext());//2
+            Console.WriteLine(gp.GetNext());//4
+            Console.WriteLine(gp.GetNext());//8
+            Console.WriteLine(gp.GetNext());//16
+            gp.Reset();
+            Console.WriteLine(gp.GetNext());//2
+
+            Console.ReadKey(); 
+        }
+    }
+}
